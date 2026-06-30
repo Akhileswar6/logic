89d320b84b9c0ba2391152b99b41a02aa7ecd6c0
Akhileswar Kamale <akhilkamale@gmail.com>
2026-06-30 16:52:10 +0530

    feat: added new code

---
+++ Array_Programs/compare_arrays.py
@@ -0,0 +1,10 @@
+def compare_arrays(arr1, arr2):
+    if len(arr1) != len(arr2):
+        return False
+    
+    for i in range(len(arr1)):
+        if arr1[i] != arr2[i]:
+            return False    
+    return True
+
+print(compare_arrays([1,2,3], [1,2,3,8]))
