c74258181a527cbdfb1e51e0015625218ed36e93
Akhileswar Kamale <akhilkamale@gmail.com>
2026-06-30 16:54:45 +0530

    feat: updated code

---
+++ Array_Programs/reverse_array.py
@@ -0,0 +1,12 @@
+def reverse_array(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    
+    return arr
+
+print(reverse_array([6,9,6,3,8,1]))
