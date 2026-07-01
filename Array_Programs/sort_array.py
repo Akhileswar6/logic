2fc99216b654433d97e9e00f6d495e2c447772d4
Akhileswar Kamale <akhilkamale@gmail.com>
2026-07-01 23:22:36 +0530

    feat: added sort and sum of array

---
+++ Array_Programs/sort_array.py
@@ -0,0 +1,9 @@
+def sort_array(arr):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+
+print(sort_array([23, 23, 89, 76, 1]))
---
+++ Array_Programs/sum_of_elements_in_array.py
@@ -0,0 +1,10 @@
+def sum_array(arr):
+    result = 0
+
+    for i in range(len(arr)):
+        result += arr[i]
+
+    return result
+
+print(sum_array([23,23,89,76,1]))        
+
